547b1238ab452f74e994399e479c1048f4cdf575
Aarushi0010 <mahajanaarushi10@gmail.com>
2025-04-27 14:09:19 +0530

    path&query parameters

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from typing import Optional
 
 app = FastAPI()
 
@@ -9,3 +10,15 @@ def base():
 @app.get('/data')
 def data():
     return {'data' : {'name' : 'Aarushi'}}
+
+@app.get('/blog/{id}')
+def blog(id):
+    return 'blog at',id
+
+@app.get('/query')
+def query(limit = 10 ,published :bool = True ,sort : Optional[str] = None ):
+    if published:
+        return {'data' : f'{limit} published blogs from db'}
+    
+    else :
+        return {'data' : f'{limit} blogs from db'}
