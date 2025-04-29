0cf1b0de631028158343a0844ef21743ff1fe81a
Aarushi0010 <mahajanaarushi10@gmail.com>
2025-04-29 17:08:25 +0530

    table created

--- content/database.py
+++ content/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 
-SQLALCHEMY_DATABASE_URL = "sqlite://./content.db"
+SQLALCHEMY_DATABASE_URL = "sqlite:///content.db"
 engine = create_engine(SQLALCHEMY_DATABASE_URL , connect_args={"check_same_thread" : False})
 
 Base = declarative_base()
---
+++ content/models.py
@@ -0,0 +1,10 @@
+from sqlalchemy import Column, Integer, String
+from .database import Base
+
+
+class Content(Base):
+
+    __tablename__ = 'content'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    body = Column(String)
